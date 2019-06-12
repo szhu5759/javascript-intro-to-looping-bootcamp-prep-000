3320780d1c7f1261630d89a7828f4e3f6bb21756
szhu5759 <szhu5759@gmail.com>
2019-06-12 22:07:23 +0000

    Automatically backed up by Learn

--- sb.js
+++ sb.js
@@ -1,12 +1,11 @@
-var i = 0
+var i = -1
 function doWhileLoop(num) {
   function incrementVariable() {
     i = i + 1
     return i
 }
-
   do {
-    console.log(`${i}-I run once regardless. `)
+    console.log(`${i}I run once regardless.`)
   }  while (incrementVariable() < num)
 }
 
